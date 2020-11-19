df88580d97195fa2b735fc11423c8e2b599fac45
akeslo <adam.keslowitz@gmail.com>
2020-11-18 23:40:29 -0500

    v1.0.2 - Amex Refunds, Discover Bug Fix for Merchant Name

--- amexTransaction.php
+++ amexTransaction.php
@@ -11,25 +11,26 @@ class amexTransaction
     public function __construct(string $message)
     {
       //Set Amount
-      $amountPattern = "/\d{1,6}\.{1}\d{2}/";
-      if (preg_match_all($amountPattern, $message, $matches)) {
-         $this->amount = $matches[0][1];
+      $amountPattern = "/-{0,1}\\\$\d{1,3}\.\d{2}/";
+      if (preg_match($amountPattern, $message, $matches)) {
+         $this->amount = str_replace("$","",$matches[0]);
       }
 
       //Set Merchant
-      $merchantPattern = "/(?<=large purchase notifications online.)(.*)(?=\\\$)/";
+      $merchantPattern = "/(?<=large purchase notifications online.)(.*)(?=\\\$)|(?<=credits you received)(.*)(?=-\\\$)/";
       if (preg_match($merchantPattern, $message, $matches)) {
          $this->merchant = ucwords(strtolower($matches[0]));
       }
 
       //Set Account
-      $accountPattern = "/(?<=PLATINUM CARDACCOUNT ENDING: )\d{5}/";
+      $accountPattern = "/(?i)(?<=ACCOUNT ENDING: )\d{5}/";
       if (preg_match($accountPattern, $message, $matches)) {
          $this->account = $matches[0];
       }
 
+
       //Set Date
-      $datePattern = "/\w{3,4} \d{2}, \d{4}/";
+      $datePattern = "/(?i)[a-z]{3,4} \d{2}, \d{4}/";
       if (preg_match($datePattern, $message, $matches)) {
          $orgDate = $matches[0];
          $this->date = date("Y-m-d", strtotime($orgDate));
